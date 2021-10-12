78ed908a1eabffe29163f6e7cc03836fd3365071
redbird <redbird8941@gmail.com>
2021-10-12 19:53:46 +0300

    add converter

--- src/main/java/ImageConverter.java
+++ src/main/java/ImageConverter.java
@@ -20,6 +20,19 @@ public class ImageConverter {
     public static boolean convertFormat(String inputImagePath,
                                         String outputImagePath,
                                         String formatName) throws IOException {
-        return false;
+        FileInputStream inputStream = new FileInputStream(inputImagePath);
+        FileOutputStream outputStream = new FileOutputStream(outputImagePath);
+
+        // reads input image from file
+        BufferedImage inputImage = ImageIO.read(inputStream);
+
+        // writes to the output image in specified format
+        boolean result = ImageIO.write(inputImage, formatName, outputStream);
+
+        // needs to close the streams
+        outputStream.close();
+        inputStream.close();
+
+        return result;
     }
 }
