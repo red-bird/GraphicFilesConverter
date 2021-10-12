c6629325f4845071cf660fb204aa0bfcab52ae6d
redbird <redbird8941@gmail.com>
2021-10-12 19:50:07 +0300

    add test

---
+++ src/main/java/ImageConverter.java
@@ -0,0 +1,25 @@
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageConverter {
+
+    /**
+     * Converts an image to another format
+     *
+     * @param inputImagePath Path of the source image
+     * @param outputImagePath Path of the destination image
+     * @param formatName the format to be converted to, one of: jpeg, png,
+     * bmp, wbmp, and gif
+     * @return true if successful, false otherwise
+     * @throws IOException if errors occur during writing
+     */
+    public static boolean convertFormat(String inputImagePath,
+                                        String outputImagePath,
+                                        String formatName) throws IOException {
+        return false;
+    }
+}
